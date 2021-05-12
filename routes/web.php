508cbb015dad7862d8ebeb54c0b46d9e9f56be27
Nhân <nhannguyenduc@admicro.vn>
2021-05-12 20:29:11 +0700

    add

--- routes/web.php
+++ routes/web.php
@@ -12,16 +12,29 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//customer
 Route::get('/', 'Shop\\HomeController@index');
 Route::get('{id}/category', 'Shop\\CategoryController@index')->name('shop.category');
 Route::get('/search', 'Shop\\SearchController@search')->name('shop.search');
 Route::get('{id}/product', 'Shop\\ProductController@index')->name('shop.product');
 
-Route::get('/cart', 'Shop\\CartController@index')->name('shop.cart');
-Route::post('/add-cart', 'Shop\\CartController@addCart');
-Route::delete('/delete-cart/{id}', 'Shop\\CartController@delete');
 
+Route::middleware('user')->group(function(){
+
+    //cart
+    Route::get('/cart', 'Shop\\CartController@index')->name('shop.cart');
+    Route::post('/add-cart', 'Shop\\CartController@addCart');
+    Route::delete('/delete-cart/{id}', 'Shop\\CartController@delete');
+
+    //order
+    Route::post('/submit-order', 'Shop\\OrderController@submit');
+    Route::get('/order', 'Shop\\OrderController@index');
+    Route::post('/delete-order/{id}', 'Shop\\OrderController@delete');
+
+});
+
+
+//auth
 Route::get('/login', 'Shop\\Auth\\LoginController@index')->name('shop.login');
 Route::post('/login', 'Shop\\Auth\\LoginController@submit')->name('shop.login.submit');
 Route::get('/logout', 'Shop\\Auth\\LoginController@logout')->name('shop.login.logout');
@@ -29,6 +42,3 @@ Route::post('/sigh-up', 'Shop\\Auth\\LoginController@sighUp')->name('shop.login.
 Route::get('/checklogin', 'Shop\\Auth\\LoginController@checkLogin');
 
 
-Route::post('/submit-order', 'Shop\\OrderController@submit');
-Route::get('/order', 'Shop\\OrderController@index');
-Route::post('/delete-order/{id}', 'Shop\\OrderController@delete');
